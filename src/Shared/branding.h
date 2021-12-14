60fb169c84ab4f1ee638de25aff3748ee3c69cef
jugglinmike <mike@mikepennisi.com>
2021-12-14 16:51:33 -0500

    Centralize definition of brand identifiers (#7)
    * Centralize definition of brand identifiers
    
    Prior to this commit, the name of the Automation Voice and its "vendor"
    were defined on an ad-hoc basis. This approach made it difficult to
    understand how the brand information was used across the Visual Studio
    projects, and this in turn made it difficult to infer and maintain
    relationships between the values.
    
    Introduce a Visual Studio "Shared Project" to define all brand
    identifiers and reference those definitions consistently.
    
    * Reuse shared file
    
    * Remove import for unused library

---
+++ src/Shared/branding.h
@@ -0,0 +1,5 @@
+#pragma once
+#define AUTOMATION_VOICE_ID "BocoupAutomationVoice"
+#define AUTOMATION_VOICE_NAME "Bocoup Automation Voice"
+#define AUTOMATION_VOICE_VENDOR "Bocoup"
+#define AUTOMATION_VOICE_HOME "C:\\Program Files\\Bocoup Automation Voice"
