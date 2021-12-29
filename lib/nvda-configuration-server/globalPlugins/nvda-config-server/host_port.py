9a70ddd888c39c6b96138897a17e9b31a4558ae6
Mike Pennisi <mike@mikepennisi.com>
2021-12-29 16:08:13 -0500

    Make the TCP port of the NVDA add-on configurable

--- lib/nvda-configuration-server/globalPlugins/nvda-config-server/host_port.py
+++ lib/nvda-configuration-server/globalPlugins/nvda-config-server/host_port.py
@@ -1,15 +1,18 @@
 import json
 import os
 
-# The installation procedure inserts this file from the project's "shared"
-# directory.
-filename = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    '..',
-    '..',
-    'shared',
-    'default-at-configuration-port.json'
-)
+try:
+    HOST_PORT = int(os.environ['NVDA_CONFIGURATION_SERVER_PORT'])
+except KeyError:
+    # The installation procedure inserts this file from the project's "shared"
+    # directory.
+    filename = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        '..',
+        '..',
+        'shared',
+        'default-at-configuration-port.json'
+    )
 
-with open(filename, 'r') as handle:
-    HOST_PORT = json.loads(handle.read())
+    with open(filename, 'r') as handle:
+        HOST_PORT = json.loads(handle.read())
