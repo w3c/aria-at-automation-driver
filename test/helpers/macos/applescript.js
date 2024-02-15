03a5345b9f2f0338dacece998908c73469b052b4
Michael "Z" Goddard <mzgoddard@gmail.com>
2024-02-15 00:39:39 -0500

    fixup! add macos keyboard support

--- test/helpers/macos/applescript.js
+++ test/helpers/macos/applescript.js
@@ -8,119 +8,115 @@ const {
   renderScript,
   retryOnTimeout,
   runScript,
+  APPLESCRIPT_TIMEOUT_ERROR,
 } = require('../../../lib/helpers/macos/applescript.js');
 const { parseCodePoints } = require('../../../lib/helpers/macos/parseCodePoints.js');
 const { keyCodeCommand } = require('../../../lib/helpers/macos/keyCodeCommand.js');
 
 suite('helpers/macos/applescript', () => {
-  testEach(
-    'parseCodePoints',
-    ({ description }) => description,
-    ({ keyCombination, command, throws }) => {
-      if (throws) {
-        assert.throws(() => parseCodePoints(keyCombination), throws);
-      } else {
-        assert.deepEqual(parseCodePoints(keyCombination), command);
-      }
-    },
-    [
-      {
-        description: 'space',
-        keyCombination: [' '],
-        command: keyCodeCommand(['space'], []),
-      },
-      {
-        description: 'option-a',
-        keyCombination: ['\ue00a', 'a'],
-        command: keyCodeCommand(['a'], ['option']),
-      },
-      {
-        description: 'command-option-shift-b',
-        keyCombination: ['\ue03d', '\ue00a', '\ue008', 'b'],
-        command: keyCodeCommand(['b'], ['command', 'option', 'shift']),
-      },
-      { description: 'throws on null', keyCombination: ['\ue000'], throws: /unknown key/ },
-      {
-        description: 'throws on invalid code',
-        keyCombination: ['df daf% ?'],
-        throws: /Invalid key code/,
-      },
-    ],
-  );
+  suite('parseCodePoints', () => {
+    test('space', () => {
+      assert.deepEqual(parseCodePoints([' ']), keyCodeCommand(['space'], []));
+    });
 
-  testEach(
-    'renderScript',
-    ({ description }) => description,
-    ({ command, script }) => {
-      assert.deepEqual(renderScript(command), applescript(script));
-    },
-    [
-      {
-        description: 'space',
-        command: keyCodeCommand(['space'], []),
-        script:
+    test('option-a', () => {
+      assert.deepEqual(parseCodePoints(['\ue00a', 'a']), keyCodeCommand(['a'], ['option']));
+    });
+
+    test('command-option-shift-b', () => {
+      assert.deepEqual(
+        parseCodePoints(['\ue03d', '\ue00a', '\ue008', 'b']),
+        keyCodeCommand(['b'], ['command', 'option', 'shift']),
+      );
+    });
+
+    test('throws on null', () => {
+      assert.throws(() => parseCodePoints(['\ue000']), /unknown key/);
+    });
+
+    test('throws on invalid code', () => {
+      assert.throws(() => parseCodePoints(['df daf% ?']), /Invalid key code/);
+    });
+  });
+
+  suite('renderScript', () => {
+    test('space', () => {
+      assert.deepEqual(
+        renderScript(keyCodeCommand(['space'], [])),
+        applescript(
           'with timeout of 5 seconds\n' +
-          '    tell application "System Events"\n' +
-          '        key code 49\n' +
-          '    end tell\n' +
-          'end timeout',
-      },
-      {
-        description: 'option-a',
-        command: keyCodeCommand(['a'], ['option']),
-        script:
+            '    tell application "System Events"\n' +
+            '        key code 49\n' +
+            '    end tell\n' +
+            'end timeout',
+        ),
+      );
+    });
+
+    test('option-a', () => {
+      assert.deepEqual(
+        renderScript(keyCodeCommand(['a'], ['option'])),
+        applescript(
           'with timeout of 5 seconds\n' +
-          '    tell application "System Events"\n' +
-          '        key code 0 using option down\n' +
-          '    end tell\n' +
-          'end timeout',
-      },
-      {
-        description: 'command-option-shift-b',
-        command: keyCodeCommand(['b'], ['command', 'option', 'shift']),
-        script:
+            '    tell application "System Events"\n' +
+            '        key code 0 using option down\n' +
+            '    end tell\n' +
+            'end timeout',
+        ),
+      );
+    });
+
+    test('command-option-shift-b', () => {
+      assert.deepEqual(
+        renderScript(keyCodeCommand(['b'], ['command', 'option', 'shift'])),
+        applescript(
           'with timeout of 5 seconds\n' +
-          '    tell application "System Events"\n' +
-          '        key code 11 using {command down, option down, shift down}\n' +
-          '    end tell\n' +
-          'end timeout',
-      },
-    ],
-  );
+            '    tell application "System Events"\n' +
+            '        key code 11 using {command down, option down, shift down}\n' +
+            '    end tell\n' +
+            'end timeout',
+        ),
+      );
+    });
+  });
 
   test('execScript', async () => {
     assert.equal(await execScript(applescript('return "hello"')), 'hello\n');
   });
 
-  test('retryOnTimeout', async () => {
-    let i = 0;
-    assert.equal(
-      await retryOnTimeout(async () => {
-        if (i++ === 0) {
-          throw new Error('AppleEvent timed out');
-        }
-        return 'retried';
-      }),
-      'retried',
-    );
-    assert.equal(i, 2);
+  suite('retryOnTimeout', () => {
+    test('retries on time out', async () => {
+      let i = 0;
+      assert.equal(
+        await retryOnTimeout(async () => {
+          if (i++ === 0) {
+            await new Promise(resolve => setTimeout(resolve, 1));
+            throw new Error(APPLESCRIPT_TIMEOUT_ERROR);
+          }
+          return 'retried';
+        }),
+        'retried',
+      );
+      assert.equal(i, 2);
+    });
+
+    test('rethrows unexpected error', async () => {
+      await assert.rejects(async () => {
+        await retryOnTimeout(async () => {
+          throw new Error('Not a time out');
+        });
+      }, /Not a time out/);
+    });
+
+    test('rethrows timeout after too many retries', async () => {
+      await assert.rejects(async () => {
+        await retryOnTimeout(async () => {
+          throw new Error(APPLESCRIPT_TIMEOUT_ERROR);
+        });
+      }, new RegExp(APPLESCRIPT_TIMEOUT_ERROR));
+    });
   });
 
   test('runScript', async () =>
     assert.equal(await runScript(applescript('return "hello"')), 'hello\n'));
 });
-
-/**
- * @param {string} name
- * @param {function(T, number): string} nameDelta
- * @param {function(T, number): Promise<void> | void} testfn
- * @param {T[]} tests
- * @template T
- */
-function testEach(name, nameDelta, testfn, tests) {
-  for (let i = 0; i < tests.length; i++) {
-    test(`${name}: ${nameDelta(tests[i], i)}`, async () => {
-      await testfn(tests[i], i);
-    });
-  }
-}
